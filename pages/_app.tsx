f408104eddba74aa8bae3070c25c0e9ae6fcab34
David Michael II <knowdavidmichael@gmail.com>
2022-09-23 12:01:13 +0100

    add styletron provider and base provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import "../styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
+import { Provider as StyletronProvider } from "styletron-react";
+import { BaseProvider, DarkTheme } from "baseui";
+import styletron from "../styletron";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StyletronProvider value={styletron}>
+      <BaseProvider theme={DarkTheme}>
+        <Component {...pageProps} />
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
 
 export default MyApp;
