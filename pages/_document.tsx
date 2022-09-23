6a007a7f5a6e2b600b8d926e60f10469631a646d
David Michael II <knowdavidmichael@gmail.com>
2022-09-23 11:58:27 +0100

    config: add _document config

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import * as React from "react";
+// @ts-ignore
+import { Server, Sheet } from "styletron-engine-atomic";
+import styletron from "../styletron";
+import { Provider as StyletronProvider } from "styletron-react";
+
+class MyDocument extends Document<{ stylesheets: Sheet[] }> {
+  static getInitialProps(props: any) {
+    // eslint-disable-next-line react/display-name
+    const page = props.renderPage((App: any) => (props: any) => (
+      <StyletronProvider value={styletron}>
+        <App {...props} />
+      </StyletronProvider>
+    ));
+    const stylesheets = (styletron as Server).getStylesheets() || [];
+    return { ...page, stylesheets };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {this.props.stylesheets.map((sheet, i) => (
+            <style
+              className="_styletron_hydrate_"
+              dangerouslySetInnerHTML={{ __html: sheet.css }}
+              media={sheet.attrs.media}
+              data-hydrate={sheet.attrs["data-hydrate"]}
+              key={i}
+            />
+          ))}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
