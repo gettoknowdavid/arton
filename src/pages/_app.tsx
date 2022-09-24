2267e88900e42d240222f05a5814ee13b4f7e16b
David Michael II <knowdavidmichael@gmail.com>
2022-09-24 10:17:02 +0100

    wrap Component with getLayout function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import type { AppProps } from "next/app";
 import { Provider as StyletronProvider } from "styletron-react";
 import { BaseProvider } from "baseui";
 import styletron from "../styletron";
 import { ArtonTheme } from "../theme";
 import "../styles/globals.css";
+import { AppPropsWithLayout } from "../types";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function ArtonApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <StyletronProvider value={styletron}>
       <BaseProvider theme={ArtonTheme}>
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </BaseProvider>
     </StyletronProvider>
   );
 }
 
-export default MyApp;
+export default ArtonApp;
