9b868a473206c44c983b494390d6bbcc39a83053
David Michael II <knowdavidmichael@gmail.com>
2022-09-24 09:50:11 +0100

    add custom baseweb theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { Provider as StyletronProvider } from "styletron-react";
-import { BaseProvider, DarkTheme } from "baseui";
+import { BaseProvider } from "baseui";
 import styletron from "../styletron";
+import { ArtonTheme } from "../theme";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyletronProvider value={styletron}>
-      <BaseProvider theme={DarkTheme}>
+      <BaseProvider theme={ArtonTheme}>
         <Component {...pageProps} />
       </BaseProvider>
     </StyletronProvider>
