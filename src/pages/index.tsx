8c1c37a47029f919f2c6f7ecd1c4f830315363dc
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 22:15:16 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
 import { useStyletron } from "baseui";
-import { NextPageWithLayout } from "../types";
+import { CategoryType, NextPageWithLayout } from "../types";
 import { ReactElement } from "react";
 import Layout from "../components/layout";
 import { DisplaySmall } from "baseui/typography";
 import SEO from "../components/seo";
-import { gql, useQuery } from "@apollo/client";
-import { GetStaticPropsContext } from "next";
-import { initializeApollo } from "../lib/apollo";
 import { fetchAPI } from "../lib/api";
 import { CategoriesQuery } from "../graphql";
 
-const Home: NextPageWithLayout = ({ result }) => {
+type HomeProps = {
+  categories: CategoryType[];
+};
+
+const Home: NextPageWithLayout | any = (props: HomeProps) => {
   const [css, theme] = useStyletron();
-  console.log(result);
+  console.log(props.categories);
 
   return (
     <div>
@@ -32,11 +33,11 @@ Home.getLayout = function getLayout(page: ReactElement) {
 };
 
 export async function getStaticProps() {
-  const result = await fetchAPI({ query: CategoriesQuery });
+  const { data: categories } = await fetchAPI({ query: CategoriesQuery });
 
   return {
     props: {
-      result,
+      categories,
     },
   };
 }
