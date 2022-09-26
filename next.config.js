9a0836f555cf4bd017451590e74a75b71a9c0cec
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 21:39:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:any*",
-        destination: "/",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
