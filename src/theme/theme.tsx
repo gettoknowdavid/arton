aa7fef39435545515c59eb576151b5aa2f3d07a4
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 21:44:02 +0100

    change font to Poppins

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -3,7 +3,7 @@ import { createLightTheme, lightThemePrimitives } from "baseui";
 export const ArtonTheme = createLightTheme(
   {
     ...lightThemePrimitives,
-    primaryFontFamily: '"Helvetica Neue", sans-serif',
+    primaryFontFamily: '"Poppins", cursive',
   },
   {}
 );
