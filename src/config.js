1d93ec5139881b42514fdacafa09c95ed3d6ef4c
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 21:42:35 +0100

    config: create config file

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: "ARTON®",
+  author: {
+    name: "David Michael II",
+  },
+  description:
+    "Enter the world of ARTON®: discover the latest Collections and new Collaborations. Shop your favorite brands and enjoy Free Returns and International Shipping.",
+  social: {
+    facebook: "artonhq",
+    twitter: "artonhq",
+  },
+};
