b20e10b8519ac995512552416a33034a91377aa3
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 21:43:31 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,66 @@ import { NextPageWithLayout } from "../types";
 import { ReactElement } from "react";
 import Layout from "../components/layout";
 import { DisplaySmall } from "baseui/typography";
+import SEO from "../components/seo";
+import { gql, useQuery } from "@apollo/client";
+import { GetStaticPropsContext } from "next";
+import { initializeApollo } from "../lib/apollo";
+import { fetchAPI } from "../lib/api";
 
-const Home: NextPageWithLayout = () => {
+const ImageFragment = gql`
+  fragment ImageFragment on UploadFileEntity {
+    id
+    attributes {
+      alternativeText
+      url
+    }
+  }
+`;
+
+const CategoryFragment = gql`
+  fragment CategoryFragment on CategoryEntity {
+    id
+    attributes {
+      name
+      slug
+      variant
+      image {
+        data {
+          ...ImageFragment
+        }
+      }
+    }
+  }
+`;
+
+const CategoriesQuery = gql`
+  query CategoriesQuery {
+    categories {
+      data {
+        id
+        attributes {
+          name
+          slug
+          variant
+          image {
+            data {
+              id
+              attributes {
+                alternativeText
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Home: NextPageWithLayout = ({ result }) => {
   const [css, theme] = useStyletron();
+  console.log(result);
+
   return (
     <div>
       <DisplaySmall>Home</DisplaySmall>
@@ -15,7 +72,17 @@ const Home: NextPageWithLayout = () => {
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
+  return <Layout seo={<SEO title={"Home"} description={""} />}>{page}</Layout>;
 };
+
+export async function getStaticProps() {
+  const result = await fetchAPI({ query: CategoriesQuery });
+
+  return {
+    props: {
+      result,
+    },
+  };
+}
 
 export default Home;
