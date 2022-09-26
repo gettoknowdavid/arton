aa8dec0a5c19864e2a11d3ce855307cc73111e0f
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 21:43:27 +0100

    add google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,6 +32,12 @@ class MyDocument extends Document<{ stylesheets: Sheet[] }> {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;500;700&display=swap"
+            rel="stylesheet"
+          />
           {this.props.stylesheets.map((sheet, i) => (
             <style
               className="_styletron_hydrate_"
