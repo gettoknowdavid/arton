cc27cdedfb65d84f39748dac8d16d6ec6f484e06
David Michael II <knowdavidmichael@gmail.com>
2022-09-26 23:10:48 +0100

    add maxWidth

--- src/components/layout/layout.styles.tsx
+++ src/components/layout/layout.styles.tsx
@@ -2,14 +2,17 @@ import { styled } from "baseui";
 
 export const StyledLayout = styled("div", () => ({
   maxWidth: "1920px",
+  margin: "0 auto",
   width: "100%",
   height: "100%",
-  transitionProperty: "background-color",
+  transitionProperty: "all",
   transitionDuration: ".9s",
   transitionTimingFunction: "cubic-bezier(0.1, 0.4, 0.2, 1)",
 }));
 
 export const StyledMain = styled("main", ({ $theme }) => ({
+  maxWidth: "1920px",
+  margin: "0 auto",
   position: "relative",
   width: "100%",
   [$theme.mediaQuery.small]: {},
