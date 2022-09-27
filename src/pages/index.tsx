bb42650b5fcc8ac22c9198db206cce498eba9a01
David Michael II <knowdavidmichael@gmail.com>
2022-09-27 17:16:18 +0100

    update Home page
    update HomeProps, update query with menCategories, womenCategories and heroImage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,124 @@ import { useStyletron } from "baseui";
 import { CategoryType, NextPageWithLayout } from "../types";
 import { ReactElement } from "react";
 import Layout from "../components/layout";
-import { DisplaySmall } from "baseui/typography";
 import SEO from "../components/seo";
 import { fetchAPI } from "../lib/api";
-import { CategoriesQuery } from "../graphql";
+import Image from "next/image";
+import heroImage from "../public/hero-1.jpg";
+import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
+import { HomeQuery } from "../graphql/queries/home.query";
 
 type HomeProps = {
-  categories: CategoryType[];
+  heroImage: {
+    data: {
+      id: string;
+      attributes: {
+        alternativeText: string;
+        url: string;
+      };
+    };
+  };
+  menCategories: {
+    data: CategoryType[];
+  };
+  womenCategories: {
+    data: CategoryType[];
+  };
 };
 
 const Home: NextPageWithLayout | any = (props: HomeProps) => {
   const [css, theme] = useStyletron();
-  console.log(props.categories);
+  const hero = props.heroImage.data.attributes;
+  const menCategories = props.menCategories.data;
+  const womenCategories = props.womenCategories.data;
 
   return (
     <div>
-      <DisplaySmall>Home</DisplaySmall>
+      <div
+        className={css({
+          paddingLeft: "2rem",
+          paddingRight: "2rem",
+          maxWidth: "1920px",
+          margin: "0 auto",
+          position: "relative",
+        })}
+      >
+        <div
+          className={css({
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          })}
+        >
+          <div
+            className={css({
+              height: "61.714rem",
+              width: "109.714rem",
+            })}
+          >
+            <img
+              src={hero.url}
+              alt={hero.alternativeText}
+              className={css({ width: "100%" })}
+            />
+          </div>
+        </div>
+        <FlexGrid
+          flexGridColumnCount={3}
+          flexGridColumnGap={"2px"}
+          paddingTop={"2rem"}
+          paddingBottom={"2rem"}
+        >
+          {menCategories.map((category: CategoryType) => (
+            <FlexGridItem
+              key={category.id}
+              className={css({
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignContent: "center",
+                textAlign: "center",
+                cursor: "pointer",
+              })}
+            >
+              <img
+                src={category.attributes.image.data.attributes.url}
+                alt={category.attributes.image.data.attributes.alternativeText}
+                className={css({
+                  height: "61.714rem",
+                  objectFit: "cover",
+                  display: "block",
+                  width: "100%",
+                })}
+              />
+              <h2
+                className={css({
+                  fontSize: "1.367rem",
+                  marginBottom: "4px",
+                  fontWeight: 500,
+                  textTransform: "uppercase",
+                  letterSpacing: "2px",
+                })}
+              >
+                {category.attributes.name}
+              </h2>
+              <h2
+                className={css({
+                  margin: 0,
+                  fontSize: "1rem",
+                  fontWeight: 400,
+                  textTransform: "uppercase",
+                  letterSpacing: "2px",
+                })}
+              >
+                {`in men`}
+              </h2>
+            </FlexGridItem>
+          ))}
+        </FlexGrid>
+      </div>
     </div>
   );
 };
@@ -33,11 +135,13 @@ Home.getLayout = function getLayout(page: ReactElement) {
 };
 
 export async function getStaticProps() {
-  const { data: categories } = await fetchAPI({ query: CategoriesQuery });
+  const { data } = await fetchAPI({ query: HomeQuery });
 
   return {
     props: {
-      categories,
+      heroImage: data.home.data.attributes.heroImage,
+      menCategories: data.home.data.attributes.menCategories,
+      womenCategories: data.home.data.attributes.womenCategories,
     },
   };
 }
