f6f398181c1198b426594d8de9a2a84a7c1b28aa
David Michael II <knowdavidmichael@gmail.com>
2022-09-27 02:45:00 +0100

    add StyledActionItem

--- src/components/organisms/header/header.styles.tsx
+++ src/components/organisms/header/header.styles.tsx
@@ -32,17 +32,33 @@ export const StyledHeaderLogo = styled("div", () => ({
   display: "block",
   alignItems: "center",
   justifyContent: "center",
-  height: "2.8125rem",
+  height: "2.5rem",
   position: "absolute",
   left: "50%",
   WebkitTransform: "translateX(-50%)",
   transform: "translateX(-50%)",
-  width: "8.5625rem",
+  width: "18rem",
+  cursor: "pointer",
 }));
 
 export const StyledNavItem = styled("li", ({ $theme }) => ({
   display: "list-item",
   marginRight: "2.6rem",
+  textTransform: "uppercase",
+  letterSpacing: "2px",
+  fontWeight: 400,
+  cursor: "pointer",
+  transitionProperty: "color",
+  transitionDuration: $theme.animation.timing400,
+  ":hover": {
+    color: $theme.colors.mono600,
+  },
+}));
+
+export const StyledActionItem = styled("li", ({ $theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  marginLeft: "2.6rem",
   textTransform: "uppercase",
   letterSpacing: "2px",
   fontWeight: 400,
