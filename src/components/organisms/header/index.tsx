78317a502b9884e3e3ef7b1f490f608e7d9748e5
David Michael II <knowdavidmichael@gmail.com>
2022-09-27 02:18:30 +0100

    add header with nav links

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -2,17 +2,41 @@ import React from "react";
 import { useStyletron } from "baseui";
 import Image from "next/image";
 import logo from "../../../public/logo.svg";
-import { StyledHeader, StyledHeaderLogo } from "./header.styles";
+import { StyledHeader, StyledHeaderLogo, StyledNavItem } from "./header.styles";
 
 function Header() {
   const [css, theme] = useStyletron();
 
   return (
     <StyledHeader>
-      <div>This is Arton</div>
+      <div>
+        <div>
+          <nav className={css({})}>
+            <ul
+              className={css({
+                margin: 0,
+                padding: 0,
+                listStyleType: "none",
+                display: "flex",
+              })}
+            >
+              <StyledNavItem>
+                <a>New Arrivals</a>
+              </StyledNavItem>
+              <StyledNavItem>
+                <a>Women</a>
+              </StyledNavItem>
+              <StyledNavItem>
+                <a>Men</a>
+              </StyledNavItem>
+            </ul>
+          </nav>
+        </div>
+      </div>
       <StyledHeaderLogo>
         <Image src={logo} alt={"Arton logo"} layout={"fill"} />
       </StyledHeaderLogo>
+      <div>This is Arton</div>
     </StyledHeader>
   );
 }
