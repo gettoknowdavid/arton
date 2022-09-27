3d16d1040d14dba2a32dcbc10e2268f7eb86843b
David Michael II <knowdavidmichael@gmail.com>
2022-09-27 03:17:00 +0100

    active active link styling

--- src/components/atoms/nav-item/index.tsx
+++ src/components/atoms/nav-item/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import Link from "next/link";
 import { StyledNavItem } from "./nav-item.styles";
 import { NavItemType } from "../../../types";
+import { useRouter } from "next/router";
 
 function NavItem({ item }: { item: NavItemType }) {
+  const router = useRouter();
+
   return (
-    <StyledNavItem>
+    <StyledNavItem $isActive={router.pathname === `/${item.slug}`}>
       <Link href={`/${item.slug}`}>{item.title}</Link>
     </StyledNavItem>
   );
--- src/components/atoms/nav-item/nav-item.styles.tsx
+++ src/components/atoms/nav-item/nav-item.styles.tsx
@@ -1,12 +1,19 @@
-import { styled } from "baseui";
+import { styled, Theme } from "baseui";
 
-export const StyledNavItem = styled("li", ({ $theme }) => ({
+type CustomTheme = Theme & { extraProp: string };
+
+export const StyledNavItem: any = styled<
+  "li",
+  { $isActive: boolean },
+  CustomTheme
+>("li", ({ $theme, $isActive }) => ({
   display: "list-item",
   marginRight: "2.6rem",
   textTransform: "uppercase",
   letterSpacing: "2px",
   fontWeight: 400,
   cursor: "pointer",
+  color: $isActive ? $theme.colors.mono600 : $theme.colors.mono1000,
   transitionProperty: "color",
   transitionDuration: $theme.animation.timing400,
   ":hover": {
