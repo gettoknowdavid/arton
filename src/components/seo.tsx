205097fc61d2c0f6e48231214d983325b98fe7fc
David Michael II <knowdavidmichael@gmail.com>
2022-09-27 02:18:45 +0100

    add favicon links

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -17,6 +17,26 @@ function SEO({ title, description }: { title: string; description: string }) {
       <meta property="twitter:creator" content={config.social.twitter} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
+
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 }
