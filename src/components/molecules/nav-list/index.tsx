2c74cc43bd8c8fb1b9abaf62928087a148ee1f50
David Michael II <knowdavidmichael@gmail.com>
2022-09-28 22:12:06 +0100

    add AnimateSharedLayout

--- src/components/molecules/nav-list/index.tsx
+++ src/components/molecules/nav-list/index.tsx
@@ -3,16 +3,19 @@ import { StyledNavList } from "./nav-list.styles";
 import config from "../../../config";
 import NavItem from "../../atoms/nav-item";
 import { NavItemType } from "../../../types";
+import { AnimateSharedLayout } from "framer-motion";
 
 function NavList() {
   const navList = config.nav;
 
   return (
-    <StyledNavList>
-      {navList.map((item: NavItemType) => (
-        <NavItem key={item.id} item={item} />
-      ))}
-    </StyledNavList>
+    <AnimateSharedLayout>
+      <StyledNavList>
+        {navList.map((item: NavItemType) => (
+          <NavItem key={item.id} item={item} />
+        ))}
+      </StyledNavList>
+    </AnimateSharedLayout>
   );
 }
 
