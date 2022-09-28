2c4f2071ba0e297760a5ac1e8e4657fd3a4a21ac
David Michael II <knowdavidmichael@gmail.com>
2022-09-28 22:11:48 +0100

    add animation

--- src/components/molecules/home-hero/index.tsx
+++ src/components/molecules/home-hero/index.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { Skeleton } from "baseui/skeleton";
 import { HomeHeroQuery } from "../../../graphql/queries/home.query";
+import hero1 from "../../../public/hero-1.jpg";
 import {
   StyledHomeHeroContainer,
   StyledHomeHeroImageDiv,
 } from "./home-hero.styles";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useStyletron } from "baseui";
 
 function HomeHero() {
+  const [css] = useStyletron();
   const { data, loading } = useQuery(HomeHeroQuery);
   const hero = data?.home.data.attributes.heroImage.data.attributes;
+
+  const variants = {
+    hidden: { scale: 1.24 },
+    visible: { scale: 1, transition: { duration: 1.03 } },
+  };
 
   return (
     <StyledHomeHeroContainer>
@@ -18,7 +27,18 @@ function HomeHero() {
         {loading ? (
           <Skeleton height="100%" width="100%" animation />
         ) : (
-          <Image src={hero.url} alt={hero.alternativeText} layout={"fill"} />
+          <motion.div
+            initial={"hidden"}
+            animate={"visible"}
+            variants={variants}
+            className={css({
+              height: "100%",
+              width: "100%",
+              position: "relative",
+            })}
+          >
+            <Image src={hero.url} alt={hero.alternativeText} layout={"fill"} />
+          </motion.div>
         )}
       </StyledHomeHeroImageDiv>
     </StyledHomeHeroContainer>
