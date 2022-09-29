bfe9084c3ecbc85f2e5fa34be25ce2a215f02196
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 01:31:28 +0100

    add ColourType, SizeType and ProductType

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { ReactElement, ReactNode } from "react";
 import { AppProps } from "next/app";
 import { Theme } from "baseui";
+import { Image } from "phosphor-react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -31,6 +32,21 @@ export type CategoryType = {
   };
 };
 
+export type ColourType = {
+  id: number;
+  attributes: {
+    title: string;
+    code: string;
+  };
+};
+export type SizeType = {
+  id: number;
+  attributes: {
+    title: string;
+    code: string;
+  };
+};
+
 export type NavItemType = {
   id: number;
   title: string;
@@ -38,3 +54,32 @@ export type NavItemType = {
 };
 
 export type CustomStyledComponent = Theme & { extraProp: string };
+
+export type ProductType = {
+  id: number;
+  attributes: {
+    title: string;
+    description: string;
+    slug: string;
+    price: number;
+    available: boolean;
+    brand: string;
+    details: string;
+    care: string;
+    tags: {
+      id: string;
+      title: string;
+    };
+    colour: {
+      data: ColourType;
+    };
+    sizes: {
+      data: SizeType[];
+    };
+    image: ImageType;
+    images: ImageType[];
+    category?: {
+      data: CategoryType;
+    };
+  };
+};
