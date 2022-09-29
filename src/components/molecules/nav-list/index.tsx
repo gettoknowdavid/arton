4d95c8de0898eb68e16944d6421361ae30db8f91
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 11:03:04 +0100

    remove deprecated AnimateShaedLayout

--- src/components/molecules/nav-list/index.tsx
+++ src/components/molecules/nav-list/index.tsx
@@ -3,19 +3,16 @@ import { StyledNavList } from "./nav-list.styles";
 import config from "../../../config";
 import NavItem from "../../atoms/nav-item";
 import { NavItemType } from "../../../types";
-import { AnimateSharedLayout } from "framer-motion";
 
 function NavList() {
   const navList = config.nav;
 
   return (
-    <AnimateSharedLayout>
-      <StyledNavList>
-        {navList.map((item: NavItemType) => (
-          <NavItem key={item.id} item={item} />
-        ))}
-      </StyledNavList>
-    </AnimateSharedLayout>
+    <StyledNavList>
+      {navList.map((item: NavItemType) => (
+        <NavItem key={item.id} item={item} />
+      ))}
+    </StyledNavList>
   );
 }
 
