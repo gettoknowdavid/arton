c118509f1e2e8b2005e24a8865f1d5fab9cd1eff
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 11:44:50 +0100

    add preloader for page route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,29 @@ import { AppPropsWithLayout } from "../types";
 import { useApollo } from "../lib/apollo";
 import { ApolloProvider } from "@apollo/client";
 import { AnimatePresence } from "framer-motion";
+import { Router } from "next/router";
+import LoadingScreen from "../components/organisms/loading-screen";
 
 function ArtonApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   const apolloClient = useApollo(pageProps);
+
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    const start = () => setLoading(true);
+
+    const end = () => setLoading(false);
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
 
   return (
     <ApolloProvider client={apolloClient}>
@@ -22,7 +41,11 @@ function ArtonApp({ Component, pageProps }: AppPropsWithLayout) {
       >
         <StyletronProvider value={styletron}>
           <BaseProvider theme={ArtonTheme}>
-            {getLayout(<Component {...pageProps} />)}
+            {loading ? (
+              <LoadingScreen loading={true} />
+            ) : (
+              getLayout(<Component {...pageProps} />)
+            )}
           </BaseProvider>
         </StyletronProvider>
       </AnimatePresence>
