9f29769934cd9aaab943c3518bc009ce153dfebc
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 01:38:01 +0100

    refactor: ProductItem comp

--- src/pages/men.tsx
+++ src/pages/men.tsx
@@ -12,11 +12,9 @@ import { fetchAPI } from "../lib/api";
 import { MenQuery } from "../graphql/queries/men.query";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import { useStyletron } from "baseui";
-import Image from "next/image";
-import { currency } from "../lib/currency-formatter";
-import { Skeleton } from "baseui/skeleton";
 import { ButtonGroup, SIZE, SHAPE } from "baseui/button-group";
 import { Button } from "baseui/button";
+import ProductItem from "../components/atoms/product-item";
 
 type MenProps = {
   loading: boolean;
@@ -130,70 +128,7 @@ const Men: NextPageWithLayout | any = (props: MenProps) => {
           >
             {products.map((product) => (
               <FlexGridItem key={product.id}>
-                <div
-                  className={css({
-                    aspectRatio: 12 / 16,
-                    cursor: "pointer",
-                    position: "relative",
-                    backgroundColor: theme.colors.mono200,
-                  })}
-                >
-                  {props.loading ? (
-                    <Skeleton height="100%" width="100%" animation />
-                  ) : (
-                    <Image
-                      src={product.attributes.image.data.attributes.url}
-                      alt={
-                        product?.attributes.image.data.attributes
-                          .alternativeText
-                      }
-                      className={css({ objectFit: "cover" })}
-                      layout={"fill"}
-                    />
-                  )}
-                </div>
-                <div>
-                  <p
-                    className={css({
-                      marginTop: "4px",
-                      marginBottom: 0,
-                      padding: 0,
-                      textTransform: "uppercase",
-                      fontWeight: 300,
-                      fontSize: "1rem",
-                      lineHeight: "1.3rem",
-                      height: "1rem",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      display: "-webkit-box",
-                      lineClamp: 1,
-                      WebkitLineClamp: 1,
-                      WebkitBoxOrient: "vertical",
-                    })}
-                  >
-                    {props.loading ? (
-                      <Skeleton height="1rem" width="100%" animation />
-                    ) : (
-                      product.attributes.title
-                    )}
-                  </p>
-
-                  <p
-                    className={css({
-                      marginTop: "6px",
-                      padding: 0,
-                      textTransform: "uppercase",
-                      fontSize: "1rem",
-                      lineHeight: "1.3rem",
-                    })}
-                  >
-                    {props.loading ? (
-                      <Skeleton height="1rem" width="12rem" animation />
-                    ) : (
-                      currency.format(product.attributes.price)
-                    )}
-                  </p>
-                </div>
+                <ProductItem product={product} />
               </FlexGridItem>
             ))}
           </FlexGrid>
