f3dd0429e74060c545efc76afa5ac0ac6482d953
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 10:29:03 +0100

    add StyledCFWrapper styled comp

--- src/components/molecules/category-filter/category-filter.styles.tsx
+++ src/components/molecules/category-filter/category-filter.styles.tsx
@@ -1,6 +1,11 @@
 import { styled } from "baseui";
 
 // Styled component Category Filter Title
+export const StyledCFWrapper = styled("div", () => ({
+  paddingBottom: "3rem",
+}));
+
+// Styled component Category Filter Title
 export const StyledCFTitle = styled("h1", () => ({
   marginTop: 0,
   marginBottom: "1rem",
@@ -8,4 +13,23 @@ export const StyledCFTitle = styled("h1", () => ({
   fontSize: "1.2rem",
   fontWeight: 600,
   textTransform: "uppercase",
+}));
+
+// Styled component Category Filter List
+export const StyledCFList = styled("ul", () => ({
+  margin: 0,
+  padding: 0,
+  listStyleType: "none",
+}));
+
+// Styled component Category Filter List Item
+export const StyledCFListItem = styled("li", ({ $theme }) => ({
+  marginBottom: 0,
+  padding: 0,
+  textTransform: "uppercase",
+  fontWeight: 300,
+  fontSize: "1rem",
+  lineHeight: "1.3rem",
+  cursor: "pointer",
+  ":hover": { color: $theme.colors.mono600 },
 }));
