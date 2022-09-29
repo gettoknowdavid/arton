bc37b588057e9678188f0299310ef5e6e051d4d9
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 11:03:33 +0100

    fix: header logo routing

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -7,30 +7,29 @@ import {
   StyledHeaderLogo,
   StyledNavList,
 } from "./header.styles";
-import Link from "next/link";
 import { MagnifyingGlass, User } from "phosphor-react";
 import NavList from "../../molecules/nav-list";
+import { useRouter } from "next/router";
 
 function Header() {
+  const router = useRouter();
+  const goHome = () => router.push("/");
+
   return (
     <StyledHeader>
       <nav>
         <NavList />
       </nav>
-      <StyledHeaderLogo>
-        <Link href={"/"} passHref>
-          <a>
-            <Image src={logo} alt={"Arton logo"} layout={"fill"} />
-          </a>
-        </Link>
+      <StyledHeaderLogo onClick={goHome}>
+        <Image src={logo} alt={"Arton logo"} layout={"fill"} priority />
       </StyledHeaderLogo>
       <div>
         <StyledNavList>
           <StyledActionItem>
-            <User size={18} weight="duotone" />
+            <User size={18} />
           </StyledActionItem>
           <StyledActionItem>
-            <MagnifyingGlass size={18} weight="duotone" />
+            <MagnifyingGlass size={18} />
           </StyledActionItem>
         </StyledNavList>
       </div>
