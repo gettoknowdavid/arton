6330aefffff8639f3bf6ff54e9e6ee5b931cb879
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 09:33:53 +0100

    refactor components
     ProductList, CategoryFilter, SizeFilter, SortFilter

--- src/pages/men.tsx
+++ src/pages/men.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
-import {
-  CategoryType,
-  NextPageWithLayout,
-  ProductType,
-  SizeType,
-} from "../types";
+import { NextPageWithLayout, ProductType } from "../types";
 import { ReactElement } from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -12,156 +7,35 @@ import { fetchAPI } from "../lib/api";
 import { MenQuery } from "../graphql/queries/men.query";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import { useStyletron } from "baseui";
-import { ButtonGroup, SIZE, SHAPE } from "baseui/button-group";
-import { Button } from "baseui/button";
-import ProductItem from "../components/atoms/product-item";
 import ProductList from "../components/molecules/product-list";
+import CategoryFilter from "../components/molecules/category-filter";
+import SizeFilter from "../components/molecules/size-filter";
+import SortFilter from "../components/molecules/sort-filter";
 
 type MenProps = {
   loading: boolean;
   products: {
     data: ProductType[];
   };
-  categories: {
-    data: CategoryType[];
-  };
-  sizes: {
-    data: SizeType[];
-  };
 };
-
-const SORT_LIST = [
-  { id: 0, name: "Price low to high" },
-  { id: 1, name: "Price high to low" },
-];
 
 const Men: NextPageWithLayout | any = (props: MenProps) => {
   const products = props.products.data;
-  const categories = [
-    { id: 90, attributes: { name: "All" } },
-    ...props.categories.data,
-  ];
 
   const [css, theme] = useStyletron();
 
   return (
     <div className={css({ paddingTop: "6rem", paddingBottom: "6rem" })}>
-      <FlexGrid flexGridColumnCount={3}>
+      <FlexGrid flexGridColumnCount={3} flexGridColumnGap={"2rem"}>
         <FlexGridItem maxWidth={"20rem"} width={"100%"}>
-          <div className={css({ paddingBottom: "3rem" })}>
-            <h1
-              className={css({
-                marginTop: 0,
-                marginBottom: "1rem",
-                padding: 0,
-                fontSize: "1.2rem",
-                fontWeight: 600,
-                textTransform: "uppercase",
-              })}
-            >
-              Categories
-            </h1>
-            <ul
-              className={css({ margin: 0, padding: 0, listStyleType: "none" })}
-            >
-              {categories.map((category) => (
-                <li
-                  key={category.id}
-                  className={css({
-                    marginBottom: 0,
-                    padding: 0,
-                    textTransform: "uppercase",
-                    fontWeight: 300,
-                    fontSize: "1rem",
-                    lineHeight: "1.3rem",
-                    cursor: "pointer",
-                    ":hover": { color: theme.colors.mono600 },
-                  })}
-                >
-                  <p>{category.attributes.name}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={css({ paddingBottom: "3rem" })}>
-            <h1
-              className={css({
-                marginTop: 0,
-                marginBottom: "1rem",
-                padding: 0,
-                fontSize: "1.2rem",
-                fontWeight: 600,
-                textTransform: "uppercase",
-              })}
-            >
-              Sizes
-            </h1>
-            <ul
-              className={css({ margin: 0, padding: 0, listStyleType: "none" })}
-            >
-              <ButtonGroup
-                size={SIZE.mini}
-                shape={SHAPE.square}
-                mode="checkbox"
-              >
-                {props.sizes.data.map((s) => (
-                  <Button
-                    key={s.id}
-                    className={css({
-                      textTransform: "uppercase",
-                      fontSize: "0.9rem",
-                    })}
-                  >
-                    <p>{s.attributes.code}</p>
-                  </Button>
-                ))}
-              </ButtonGroup>
-            </ul>
-          </div>
+          <CategoryFilter />
+          <SizeFilter />
         </FlexGridItem>
         <FlexGridItem>
-          <ProductList products={products} />
+          <ProductList products={products} loading={props.loading} />
         </FlexGridItem>
-        <FlexGridItem
-          maxWidth={"20rem"}
-          width={"100%"}
-          className={css({ textAlign: "right" })}
-        >
-          <div>
-            <h1
-              className={css({
-                marginTop: 0,
-                marginBottom: "1rem",
-                padding: 0,
-                fontSize: "1.2rem",
-                fontWeight: 600,
-                textTransform: "uppercase",
-              })}
-            >
-              SORT
-            </h1>
-            <ul
-              className={css({ margin: 0, padding: 0, listStyleType: "none" })}
-            >
-              {SORT_LIST.map((ITEM) => (
-                <li
-                  key={ITEM.id}
-                  className={css({
-                    marginBottom: 0,
-                    padding: 0,
-                    textTransform: "uppercase",
-                    fontWeight: 300,
-                    fontSize: "1rem",
-                    lineHeight: "1.3rem",
-                    cursor: "pointer",
-                    ":hover": { color: theme.colors.mono600 },
-                  })}
-                >
-                  <p>{ITEM.name}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
+        <FlexGridItem maxWidth={"20rem"} width={"100%"}>
+          <SortFilter />
         </FlexGridItem>
       </FlexGrid>
     </div>
@@ -183,8 +57,6 @@ export async function getStaticProps() {
     props: {
       loading: loading,
       products: data.products,
-      categories: data.categories,
-      sizes: data.sizes,
     },
   };
 }
