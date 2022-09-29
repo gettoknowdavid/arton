85cc7b92b5c8f466eeec01ea4f156e7f801d9051
David Michael II <knowdavidmichael@gmail.com>
2022-09-29 09:32:09 +0100

    add skeleton loader

--- src/components/molecules/product-list/index.tsx
+++ src/components/molecules/product-list/index.tsx
@@ -2,9 +2,39 @@ import React from "react";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import ProductItem from "../../atoms/product-item";
 import { ProductType } from "../../../types";
+import { useStyletron } from "baseui";
+import { Skeleton } from "baseui/skeleton";
 
 type ProductListProps = {
+  loading: boolean;
   products: ProductType[];
+};
+
+let productSkeletons = Array(8).fill(0);
+
+const ProductSkeleton = () => {
+  const [css, theme] = useStyletron();
+
+  return (
+    <div>
+      <div
+        className={css({
+          aspectRatio: 12 / 16,
+          backgroundColor: theme.colors.mono200,
+        })}
+      >
+        <Skeleton height={"100%"} width={"100%"} animation />
+      </div>
+      <div>
+        <div className={css({ marginTop: "4px" })}>
+          <Skeleton height={"1rem"} width={"100%"} animation />
+        </div>
+        <div className={css({ marginTop: "6px" })}>
+          <Skeleton height={"1rem"} width={"4rem"} animation />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 function ProductList(props: ProductListProps) {
@@ -16,11 +46,17 @@ function ProductList(props: ProductListProps) {
       paddingRight={"2px"}
       paddingLeft={"2px"}
     >
-      {props.products.map((product) => (
-        <FlexGridItem key={product.id}>
-          <ProductItem product={product} />
-        </FlexGridItem>
-      ))}
+      {props.loading
+        ? productSkeletons.map((index) => (
+            <FlexGridItem key={index}>
+              <ProductSkeleton key={index} />
+            </FlexGridItem>
+          ))
+        : props.products.map((product) => (
+            <FlexGridItem key={product.id}>
+              <ProductItem product={product} />
+            </FlexGridItem>
+          ))}
     </FlexGrid>
   );
 }
