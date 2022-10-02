8ed6b3a4ac8cd3c3024764f6da261a8e36a1e7c5
David Michael II <knowdavidmichael@gmail.com>
2022-10-02 01:23:38 +0100

    create RichText component

---
+++ src/components/atoms/rich-text/index.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from "react";
+import ReactMarkdown from "react-markdown";
+import { useStyletron } from "baseui";
+
+type RichTextProps = {
+  content: any;
+};
+
+function RichText(props: RichTextProps) {
+  const [css] = useStyletron();
+
+  return (
+    <ReactMarkdown
+      className={css({
+        fontWeight: 300,
+        margin: 0,
+        padding: 0,
+      })}
+      components={{
+        p({ children }) {
+          return <p className={css({ margin: 0 })}>{children}</p>;
+        },
+        ul({ children }) {
+          return (
+            <ul
+              className={css({
+                listStyleType: '"-  "',
+                margin: 0,
+                paddingBlock: 0,
+                paddingLeft: "2rem",
+              })}
+            >
+              {children}
+            </ul>
+          );
+        },
+      }}
+    >
+      {props.content}
+    </ReactMarkdown>
+  );
+}
+
+export default RichText;
