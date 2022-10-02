113c5b831aa0044842268dcd435c95e1bfe33e7a
David Michael II <knowdavidmichael@gmail.com>
2022-10-02 01:30:21 +0100

    refactor size list

--- src/components/molecules/size-filter/index.tsx
+++ src/components/molecules/size-filter/index.tsx
@@ -1,40 +1,23 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { SizeType } from "../../../types";
-import { SizesQuery } from "../../../graphql/queries/sizes.query";
 import {
   StyledSFListItem,
   StyledSFTitle,
   StyledSFWrapper,
 } from "./size-filter.styles";
 import { StyledSubFooterList } from "../../organisms/footer/footer.styles";
-import FilterSkeleton from "../../atoms/filter-skeleton";
+import { SIZES } from "../../../lib/sizes";
 
 function SizeFilter() {
-  const { data, loading } = useQuery(SizesQuery);
-
-  let sizeSkeletons = Array(4).fill(0);
-
-  const sizes: SizeType[] = data?.sizes.data;
-
   return (
     <StyledSFWrapper>
       <StyledSFTitle>Sizes</StyledSFTitle>
-      {loading ? (
-        sizeSkeletons.map((_, index) => (
-          <FilterSkeleton key={index.toString()} />
-        ))
-      ) : (
-        <StyledSubFooterList>
-          {sizes.map((s) => (
-            <StyledSFListItem key={s.id}>
-              <p>
-                {s.attributes.code} - {s.attributes.title}
-              </p>
-            </StyledSFListItem>
-          ))}
-        </StyledSubFooterList>
-      )}
+      <StyledSubFooterList>
+        {SIZES.map((s) => (
+          <StyledSFListItem key={s.id}>
+            <p>{s.label}</p>
+          </StyledSFListItem>
+        ))}
+      </StyledSubFooterList>
     </StyledSFWrapper>
   );
 }
