633e20694f00eba358f004fd90d92371b4b794e5
David Michael II <knowdavidmichael@gmail.com>
2022-10-02 01:23:51 +0100

    create ProdictPageImageList comp

---
+++ src/components/molecules/product-page-image-list/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Image from "next/image";
+import { styled, useStyletron } from "baseui";
+import { ImageType } from "../../../types";
+
+export const StyledPImageList = styled("ul", () => ({
+  margin: 0,
+  display: "flex",
+  flexDirection: "row-reverse",
+  listStyleType: "none",
+  height: "80vh",
+  width: "calc(((100% - 0px) / 1) - 0.5px)",
+  float: "right",
+  overflow: "auto",
+  whiteSpace: "nowrap",
+  position: "absolute",
+  left: 0,
+  top: 0,
+  bottom: 0,
+  right: 0,
+  paddingLeft: "10rem",
+}));
+
+export const StyledPImageListItem = styled("li", () => ({
+  aspectRatio: 12 / 16,
+  height: "80vh",
+  marginLeft: "2rem",
+  position: "relative",
+  width: "100%",
+}));
+
+type ProductPageImageListProps = {
+  images: ImageType[];
+};
+
+function ProductPageImageList(props: ProductPageImageListProps) {
+  const [css] = useStyletron();
+
+  return (
+    <StyledPImageList>
+      {props.images.map((image) => {
+        return (
+          <StyledPImageListItem key={image.id}>
+            <Image
+              src={image.attributes.url}
+              alt={image.attributes.alternativeText}
+              className={css({ objectFit: "cover" })}
+              layout={"fill"}
+              priority
+            />
+          </StyledPImageListItem>
+        );
+      })}
+    </StyledPImageList>
+  );
+}
+
+export default ProductPageImageList;
