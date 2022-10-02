90bb91f9a22ef1b9e3de01145f792b61d83e7078
David Michael II <knowdavidmichael@gmail.com>
2022-10-02 01:27:50 +0100

    create sizes list

---
+++ src/lib/sizes.tsx
@@ -0,0 +1,8 @@
+export const SIZES = [
+  { label: "XS - Extra Small", id: "XS" },
+  { label: "S - Small", id: "S" },
+  { label: "M - Medium", id: "M" },
+  { label: "L - Large", id: "L" },
+  { label: "XL - Extra Large", id: "XL" },
+  { label: "XXL - Extra Extra Large", id: "XXL" },
+];
