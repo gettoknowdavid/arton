dcb4901e652e1b38c6639e390bb328310f771fa5
David Michael II <knowdavidmichael@gmail.com>
2022-10-03 19:38:47 +0100

    create redux store

---
+++ src/store/index.ts
@@ -0,0 +1,45 @@
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  createStore,
+  ThunkAction,
+} from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { cartSlice } from "./slices";
+
+const cartConfig = { key: "cart", version: 1, storage };
+
+const store = configureStore({
+  reducer: combineReducers({
+    cart: persistReducer(cartConfig, cartSlice.reducer),
+  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type RootDispatch = typeof store.dispatch;
+
+export type RootThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export default store;
