9417bc9681962bd0db25337e5aba93d74a51a5d3
David Michael II <knowdavidmichael@gmail.com>
2022-10-03 21:00:17 +0100

    add handleClick

--- src/components/atoms/add-to-bag-button/index.tsx
+++ src/components/atoms/add-to-bag-button/index.tsx
@@ -1,13 +1,54 @@
 import React from "react";
 import { SIZE } from "baseui/select";
 import { Button } from "baseui/button";
+import { useRootDispatch } from "../../../hooks";
+import { BagItem, ProductType } from "../../../types";
+import { addToBag, toggleBagDrawer } from "../../../store/slices/bag.slice";
+import loadingIcon from "react-useanimations/lib/infinity";
+import UseAnimations from "react-useanimations";
 
-function AddToBagButton() {
+type Props = {
+  product: ProductType;
+};
+
+function AddToBagButton(props: Props) {
+  const [loading, setLoading] = React.useState(false);
+
+  const dispatch = useRootDispatch();
+
+  const { id, attributes } = props.product;
+
+  const bagItem: BagItem = {
+    id: id,
+    name: attributes.title,
+    image: attributes.image.data.attributes.url,
+    price: attributes.price,
+    quantity: 1,
+  };
+
+  function handleClick() {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 2000);
+    dispatch(addToBag(bagItem));
+    dispatch(toggleBagDrawer());
+    setTimeout(() => dispatch(toggleBagDrawer()), 5000);
+  }
+
   return (
     <Button
-      onClick={() => alert("click")}
+      onClick={handleClick}
       size={SIZE.compact}
+      isLoading={loading}
       overrides={{
+        LoadingSpinner: {
+          component: () => (
+            <UseAnimations
+              animation={loadingIcon}
+              strokeColor={"white"}
+              size={14}
+            />
+          ),
+        },
         BaseButton: {
           style: () => ({
             borderTopRightRadius: 0,
