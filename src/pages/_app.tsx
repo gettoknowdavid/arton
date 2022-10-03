5bf7ee830bf334a2cb0550f5115724258fa80f75
David Michael II <knowdavidmichael@gmail.com>
2022-10-03 19:39:29 +0100

    add ArtonProvider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,14 @@ import { Provider as StyletronProvider } from "styletron-react";
 import { BaseProvider } from "baseui";
 import styletron from "../styletron";
 import { ArtonTheme } from "../theme";
-import "../styles/globals.css";
 import { AppPropsWithLayout } from "../types";
 import { useApollo } from "../lib/apollo";
 import { ApolloProvider } from "@apollo/client";
 import { AnimatePresence } from "framer-motion";
 import { Router } from "next/router";
 import LoadingScreen from "../components/organisms/loading-screen";
+import ArtonProvider from "../store/providers/arton-provider";
+import "../styles/globals.css";
 
 function ArtonApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
@@ -34,21 +35,23 @@ function ArtonApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <ApolloProvider client={apolloClient}>
-      <AnimatePresence
-        mode={"wait"}
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <StyletronProvider value={styletron}>
-          <BaseProvider theme={ArtonTheme}>
-            {loading ? (
-              <LoadingScreen loading={true} />
-            ) : (
-              getLayout(<Component {...pageProps} />)
-            )}
-          </BaseProvider>
-        </StyletronProvider>
-      </AnimatePresence>
+      <ArtonProvider>
+        <AnimatePresence
+          mode={"wait"}
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <StyletronProvider value={styletron}>
+            <BaseProvider theme={ArtonTheme}>
+              {loading ? (
+                <LoadingScreen loading={true} />
+              ) : (
+                getLayout(<Component {...pageProps} />)
+              )}
+            </BaseProvider>
+          </StyletronProvider>
+        </AnimatePresence>
+      </ArtonProvider>
     </ApolloProvider>
   );
 }
