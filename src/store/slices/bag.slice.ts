726bc0cf73a18b17fd2e52f2bd737b89938e24a9
David Michael II <knowdavidmichael@gmail.com>
2022-10-04 14:33:43 +0100

    add totalAmount

--- src/store/slices/bag.slice.ts
+++ src/store/slices/bag.slice.ts
@@ -6,12 +6,14 @@ export type BagSlice = {
   items: BagItemInterface[];
   totalQuantity: number;
   bagDrawerOpen: boolean;
+  // totalAmount: number;
 };
 
 const initialState: BagSlice = {
   items: [],
   totalQuantity: 0,
   bagDrawerOpen: false,
+  // totalAmount: 0.0,
 };
 
 export const bagSlice = createSlice({
@@ -43,5 +45,12 @@ export const bagSlice = createSlice({
 export const { addToBag, toggleBagDrawer } = bagSlice.actions;
 
 export const selectBag = (state: RootState) => state.bag;
+
+export const totalAmount = (state: RootState): number =>
+  state.bag.items.reduce((bagTotal: number, currentItem: BagItemInterface) => {
+    const { price, quantity } = currentItem;
+    bagTotal += price * quantity;
+    return bagTotal;
+  }, 0);
 
 export const bagItems = (state: RootState) => state.bag.items;
