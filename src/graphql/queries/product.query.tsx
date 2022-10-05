1dc8d12489b20638c94164aea3c6bcbbc2517894
David Michael II <knowdavidmichael@gmail.com>
2022-10-05 23:21:58 +0100

    update ProductQuery

--- src/graphql/queries/product.query.tsx
+++ src/graphql/queries/product.query.tsx
@@ -1,13 +1,51 @@
 import { gql } from "@apollo/client";
-import { ProductFragment } from "../fragments";
+import { ColourFragment, ImageFragment, SizeFragment } from "../fragments";
 
 export const ProductQuery = gql`
-  ${ProductFragment}
+  ${ColourFragment}
+  ${SizeFragment}
+  ${ImageFragment}
   query ProductQuery($slug: String!) {
     products(filters: { slug: { eq: $slug } }) {
       data {
-        ...ProductFragment
+        id
         attributes {
+          title
+          description
+          slug
+          price
+          available
+          brand
+          details
+          care
+          variant
+          tags {
+            id
+            title
+          }
+          colour {
+            data {
+              ...ColourFragment
+            }
+          }
+          sizes {
+            data {
+              ...SizeFragment
+            }
+          }
+          image {
+            data {
+              attributes {
+                url
+                alternativeText
+              }
+            }
+          }
+          images {
+            data {
+              ...ImageFragment
+            }
+          }
           category {
             data {
               attributes {
