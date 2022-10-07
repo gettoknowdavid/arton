aa9fffe2afe8c7c2b2ddf9738540fc7c97db9738
David Michael II <knowdavidmichael@gmail.com>
2022-10-07 20:26:29 +0100

    refactor styling

--- src/components/organisms/drawer/drawer.styles.tsx
+++ src/components/organisms/drawer/drawer.styles.tsx
@@ -1,3 +1,5 @@
+import { styled } from "baseui";
+
 export const DrawerOverrides = {
   DrawerBody: {
     style: () => ({
@@ -9,3 +11,58 @@ export const DrawerOverrides = {
     }),
   },
 };
+
+export const StyledDrawerBody = styled("div", ({ $theme }) => ({
+  position: "relative",
+  height: "100%",
+  WebkitTransform: "translateX(0)",
+  transform: "translateX(0)",
+  overflow: "hidden",
+  width: "200%",
+  backgroundColor: $theme.colors.white,
+}));
+
+export const StyledDrawerList = styled("ul", ({ $theme }) => ({
+  display: "flex",
+  margin: 0,
+  WebkitBoxOrient: "vertical",
+  WebkitBoxDirection: "normal",
+  MsFlexDirection: "column",
+  flexDirection: "column",
+  MsFlexWrap: "nowrap",
+  flexWrap: "nowrap",
+  padding: 0,
+  listStyle: "none",
+  backgroundColor: $theme.colors.white,
+}));
+
+export const StyledDrawerListItem = styled("li", ({ $theme }) => ({
+  borderBottomWidth: "0.063rem",
+  borderBottomStyle: "solid",
+  borderBottomColor: $theme.colors.black,
+  display: "list-item",
+  textAlign: "left",
+  height: "100%",
+}));
+
+export const StyledDrawerLink = styled("a", ({ $theme }) => ({
+  ...$theme.typography.font100,
+  lineHeight: "130%",
+  letterSpacing: ".05rem",
+  textTransform: "uppercase",
+  position: "relative",
+  MsFlexPack: "left",
+  justifyContent: "left",
+  WebkitBoxAlign: "center",
+  MsFlexAlign: "center",
+  alignItems: "center",
+  width: "100%",
+  padding: "0 .75rem",
+  height: "3.75rem",
+  color: $theme.colors.black,
+  textAlign: "left",
+  border: "none",
+  background: "none",
+  textDecoration: "none",
+  display: "flex",
+}));
--- src/components/organisms/drawer/index.tsx
+++ src/components/organisms/drawer/index.tsx
@@ -5,16 +5,30 @@ import { closeDrawer, selectGlobal } from "../../../store/slices/global.slice";
 import { useStyletron } from "baseui";
 import config from "../../../config";
 import { NavItemType } from "../../../types";
-import { DrawerOverrides } from "./drawer.styles";
+import {
+  DrawerOverrides,
+  StyledDrawerBody,
+  StyledDrawerLink,
+  StyledDrawerList,
+  StyledDrawerListItem,
+} from "./drawer.styles";
 import SearchBox from "../../atoms/search-box";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Drawer() {
   const [css, theme] = useStyletron();
 
-  const navList = config.nav;
-
   const dispatch = useRootDispatch();
   const { drawerOpen } = useRootSelector(selectGlobal);
+
+  const router = useRouter();
+  const goToLink = (slug: string) => {
+    dispatch(closeDrawer());
+    router.push(`/${slug}`);
+  };
+
+  const navList = config.nav;
 
   return (
     <BaseDrawer
@@ -26,74 +40,15 @@ function Drawer() {
       overrides={DrawerOverrides}
     >
       <SearchBox />
-      <div
-        className={css({
-          position: "relative",
-          height: "100%",
-          WebkitTransform: "translateX(0)",
-          transform: "translateX(0)",
-          overflow: "hidden",
-          width: "200%",
-          backgroundColor: theme.colors.white,
-        })}
-      >
-        <ul
-          className={css({
-            display: "flex",
-            margin: 0,
-            WebkitBoxOrient: "vertical",
-            WebkitBoxDirection: "normal",
-            MsFlexDirection: "column",
-            flexDirection: "column",
-            MsFlexWrap: "nowrap",
-            flexWrap: "nowrap",
-            padding: "0",
-            listStyle: "none",
-            backgroundColor: "#fff",
-          })}
-        >
+      <StyledDrawerBody>
+        <StyledDrawerList>
           {navList.map((item: NavItemType) => (
-            <li
+            <StyledDrawerListItem
               key={item.id}
-              className={css({
-                borderBottomWidth: "0.063rem",
-                borderBottomStyle: "solid",
-                borderBottomColor: theme.colors.black,
-                display: "list-item",
-                textAlign: "left",
-                height: "100%",
-              })}
+              onClick={() => goToLink(item.slug)}
             >
-              <a
-                className={css({
-                  ...theme.typography.font100,
-                  lineHeight: "130%",
-                  letterSpacing: ".05rem",
-                  textTransform: "uppercase",
-                  WebkitTransition: "-webkit-box-shadow .15s ease-out",
-                  transition: "-webkit-box-shadow .15s ease-out",
-                  WebkitBoxShadow: "0 0 0 0 #aaaaac",
-                  boxShadow: "0 0 0 0 #aaaaac",
-                  position: "relative",
-                  MsFlexPack: "left",
-                  justifyContent: "left",
-                  WebkitBoxAlign: "center",
-                  MsFlexAlign: "center",
-                  alignItems: "center",
-                  width: "100%",
-                  padding: "0 .75rem",
-                  height: "3.75rem",
-                  color: "#000",
-                  textAlign: "left",
-                  border: "none",
-                  background: "none",
-                  textDecoration: "none",
-                  display: "flex",
-                })}
-              >
-                {item.title}
-              </a>
-            </li>
+              <StyledDrawerLink>{item.title}</StyledDrawerLink>
+            </StyledDrawerListItem>
           ))}
           <li>
             <ul
@@ -182,8 +137,8 @@ function Drawer() {
               </li>
             </ul>
           </li>
-        </ul>
-      </div>
+        </StyledDrawerList>
+      </StyledDrawerBody>
     </BaseDrawer>
   );
 }
