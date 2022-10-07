01c5e18166232a936323ded3ebccfe456702862c
David Michael II <knowdavidmichael@gmail.com>
2022-10-07 16:20:03 +0100

    refactor components

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -1,62 +1,28 @@
 import React from "react";
-import Image from "next/image";
-import logo from "../../../public/logo.svg";
 import {
   StyledActionItem,
   StyledActionsList,
-  StyledHamburger,
   StyledHeader,
-  StyledHeaderLogo,
 } from "./header.styles";
-import { List, MagnifyingGlass, User, X } from "phosphor-react";
+import { MagnifyingGlass, User } from "phosphor-react";
 import NavList from "../../molecules/nav-list";
 import BagIcon from "../../atoms/bag-icon";
-import { Block } from "baseui/block";
-import Link from "next/link";
-import { useRootDispatch, useRootSelector } from "../../../hooks";
-import { selectGlobal, toggleDrawer } from "../../../store/slices/global.slice";
+import Logo from "../../atoms/logo";
+import Hamburger from "../../atoms/hamburger";
 
 function Header() {
-  const dispatch = useRootDispatch();
-  const { drawerOpen } = useRootSelector(selectGlobal);
-  const openDrawer = () => dispatch(toggleDrawer());
-
   return (
     <header>
       <div>
         <StyledHeader>
-          {/* Logo */}
-          <StyledHeaderLogo>
-            <Link href={"/"}>
-              <a>
-                <h1>
-                  <Image
-                    src={logo}
-                    alt={"Arton logo"}
-                    layout={"fill"}
-                    priority
-                  />
-                </h1>
-              </a>
-            </Link>
-          </StyledHeaderLogo>
+          <Logo />
 
-          {/* Main Nav */}
           <nav>
-            {/* Nav list */}
-            <Block display={["none", "none", "none", "flex"]}>
-              <NavList />
-            </Block>
-
-            {/* Hamburger */}
-            <Block display={["flex", "flex", "flex", "none"]}>
-              <StyledHamburger onClick={openDrawer}>
-                {drawerOpen ? <X /> : <List />}
-              </StyledHamburger>
-            </Block>
+            <NavList />
+            <Hamburger />
           </nav>
 
-          <Block>
+          <div>
             <StyledActionsList>
               <StyledActionItem display={["none", "none", "none", "flex"]}>
                 <User />
@@ -68,7 +34,7 @@ function Header() {
                 <BagIcon />
               </StyledActionItem>
             </StyledActionsList>
-          </Block>
+          </div>
         </StyledHeader>
       </div>
     </header>
