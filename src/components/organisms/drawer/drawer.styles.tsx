1aaea33838d73cca0f496273140195c0baf727a9
David Michael II <knowdavidmichael@gmail.com>
2022-10-07 18:15:34 +0100

    Create drawer.styles.tsx

---
+++ src/components/organisms/drawer/drawer.styles.tsx
@@ -0,0 +1,11 @@
+export const DrawerOverrides = {
+  DrawerBody: {
+    style: () => ({
+      overflow: "hidden",
+      marginTop: "2.8125rem",
+      marginRight: 0,
+      marginBottom: 0,
+      marginLeft: 0,
+    }),
+  },
+};
