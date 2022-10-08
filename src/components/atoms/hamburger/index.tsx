61cb7c9de69050756637af2e0eb2318a7d8acb74
David Michael II <knowdavidmichael@gmail.com>
2022-10-08 17:32:53 +0100

    replace redux with context api

--- src/components/atoms/hamburger/index.tsx
+++ src/components/atoms/hamburger/index.tsx
@@ -2,20 +2,19 @@ import React from "react";
 import { List, X } from "phosphor-react";
 import { Block } from "baseui/block";
 import { StyledHamburger } from "./hamburger.styles";
-import { useRootDispatch, useRootSelector } from "../../../hooks";
-import { selectGlobal, toggleDrawer } from "../../../store/slices/global.slice";
+import { GlobalContext } from "../../../contexts/global.context";
 
 function Hamburger() {
-  const dispatch = useRootDispatch();
-  const { drawerOpen } = useRootSelector(selectGlobal);
-  const openDrawer = () => dispatch(toggleDrawer());
-
   return (
-    <Block display={["flex", "flex", "flex", "none"]}>
-      <StyledHamburger onClick={openDrawer}>
-        {drawerOpen ? <X /> : <List />}
-      </StyledHamburger>
-    </Block>
+    <GlobalContext.Consumer>
+      {({ toggleDrawer, drawerOpen }) => (
+        <Block display={["flex", "flex", "flex", "none"]}>
+          <StyledHamburger onClick={toggleDrawer}>
+            {drawerOpen ? <X /> : <List />}
+          </StyledHamburger>
+        </Block>
+      )}
+    </GlobalContext.Consumer>
   );
 }
 
