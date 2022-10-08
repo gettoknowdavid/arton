dedfdbf987c140aeba3074aa200f0a624d34a89c
David Michael II <knowdavidmichael@gmail.com>
2022-10-08 17:32:58 +0100

    replace redux with context api

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../../public/logo.svg";
 import { StyledLogo } from "./logo.styles";
-import { useRootDispatch } from "../../../hooks";
-import { closeDrawer } from "../../../store/slices/global.slice";
 import { useRouter } from "next/router";
+import { GlobalContext } from "../../../contexts/global.context";
 
 function Logo() {
-  const dispatch = useRootDispatch();
+  const { closeDrawer } = React.useContext(GlobalContext);
 
   const router = useRouter();
   const goHome = () => {
-    dispatch(closeDrawer());
+    closeDrawer();
     router?.push(`/`);
   };
 
