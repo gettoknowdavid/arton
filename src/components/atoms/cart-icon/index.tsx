feb4aec6abe955e78905acae2c99dfe08e77b777
David Michael II <knowdavidmichael@gmail.com>
2022-10-08 17:32:42 +0100

    remove redux

--- src/components/atoms/cart-icon/index.tsx
+++ src/components/atoms/cart-icon/index.tsx
@@ -1,28 +1,17 @@
 import React from "react";
 import { ShoppingCart } from "phosphor-react";
-import { useRootSelector } from "../../../hooks";
-import { selectCart } from "../../../store/slices/cart.slice";
 import { useStyletron } from "baseui";
 import { useRouter } from "next/router";
 import { Block } from "baseui/block";
 
 function CartIcon() {
   const [css, theme] = useStyletron();
-  const [quantity, setQuantity] = React.useState(0);
 
   const router = useRouter();
 
-  const cart = useRootSelector(selectCart);
-
-  React.useEffect(() => {
-    if (cart?.totalQuantity) {
-      setQuantity(cart.totalQuantity);
-    }
-  }, [cart]);
-
   return (
     <div
-      onClick={() => router.push("/bag")}
+      onClick={() => router.push("/cart")}
       className={css({
         position: "relative",
         display: "flex",
@@ -34,27 +23,25 @@ function CartIcon() {
       <Block display={["flex", "flex", "flex", "none"]}>
         <ShoppingCart />
       </Block>
-      {quantity > 0 ? (
-        <p
-          className={css({
-            height: "0.75rem",
-            width: "0.75rem",
-            backgroundColor: theme.colors.black,
-            color: theme.colors.white,
-            marginLeft: "0.2rem",
-            fontSize: "0.563rem",
-            fontWeight: 500,
-            letterSpacing: 0,
-            lineHeight: "0.625rem",
-            display: "flex",
-            justifyContent: "center",
-            textAlign: "center",
-            alignItems: "center",
-          })}
-        >
-          {quantity}
-        </p>
-      ) : null}
+      <p
+        className={css({
+          height: "0.75rem",
+          width: "0.75rem",
+          backgroundColor: theme.colors.black,
+          color: theme.colors.white,
+          marginLeft: "0.2rem",
+          fontSize: "0.563rem",
+          fontWeight: 500,
+          letterSpacing: 0,
+          lineHeight: "0.625rem",
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          alignItems: "center",
+        })}
+      >
+        80
+      </p>
     </div>
   );
 }
