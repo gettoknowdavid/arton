543e4186fd96c37062e45a9e1bbe25ddfc2afde5
David Michael II <knowdavidmichael@gmail.com>
2022-10-09 16:32:53 +0100

    add CartProvider

--- src/contexts/arton-provider.tsx
+++ src/contexts/arton-provider.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from "react";
 import GlobalProvider from "./global.context";
+import CartProvider from "./cart.context";
 
 type Props = {
   children: ReactNode;
 };
 
 function ArtonProvider(props: Props) {
-  return <GlobalProvider>{props.children}</GlobalProvider>;
+  return (
+    <GlobalProvider>
+      <CartProvider>{props.children}</CartProvider>
+    </GlobalProvider>
+  );
 }
 
 export default ArtonProvider;
