abfe338879b2830bb6abfd1da7d3e989f66ae8d0
David Michael II <knowdavidmichael@gmail.com>
2022-10-09 17:34:05 +0100

    refactor button styling

--- src/components/atoms/add-to-cart-button/index.tsx
+++ src/components/atoms/add-to-cart-button/index.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { SIZE } from "baseui/select";
-import { Button } from "baseui/button";
 import { CartActionType, CartItemInterface } from "../../../types";
-import UseAnimations from "react-useanimations";
-import loadingIcon from "react-useanimations/lib/infinity";
 import { CartContext } from "../../../contexts/cart.context";
+import Button from "../button";
 
-type Props = {
-  item: CartItemInterface;
-};
-
-function AddToCartButton(props: Props) {
+function AddToCartButton(item: CartItemInterface) {
   const [loading, setLoading] = React.useState(false);
-
-  const bagItem = props.item;
 
   const { dispatch } = React.useContext(CartContext);
 
@@ -21,7 +13,7 @@ function AddToCartButton(props: Props) {
     setLoading(true);
     dispatch({
       type: CartActionType.ADD_TO_CART,
-      payload: { item: bagItem },
+      payload: { item: item },
     });
     dispatch({ type: CartActionType.TOGGLE_CART_DRAWER });
     setTimeout(
@@ -32,30 +24,7 @@ function AddToCartButton(props: Props) {
   };
 
   return (
-    <Button
-      onClick={onClick}
-      isLoading={loading}
-      size={SIZE.compact}
-      overrides={{
-        LoadingSpinner: {
-          component: () => (
-            <UseAnimations
-              animation={loadingIcon}
-              strokeColor={"white"}
-              size={15}
-            />
-          ),
-        },
-        BaseButton: {
-          style: ({ $theme }) => ({
-            ...$theme.typography.font100,
-            letterSpacing: ".05rem",
-            width: "100%",
-            textTransform: "uppercase",
-          }),
-        },
-      }}
-    >
+    <Button onClick={onClick} isLoading={loading} size={SIZE.compact}>
       Add to Cart
     </Button>
   );
