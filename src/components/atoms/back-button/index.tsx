1c3ec78bef31767b9fe5d8df41208ae929918748
David Michael II <knowdavidmichael@gmail.com>
2022-10-09 17:30:19 +0100

    refactor styling

--- src/components/atoms/back-button/index.tsx
+++ src/components/atoms/back-button/index.tsx
@@ -1,52 +1,44 @@
 import React from "react";
 import Image from "next/image";
 import leftArrow from "../../../public/left-arrow.png";
-import { useStyletron } from "baseui";
+import { styled } from "baseui";
 import { useRouter } from "next/router";
 
+const Wrapper = styled("div", () => ({
+  position: "relative",
+  height: "1.3rem",
+  width: "100%",
+  cursor: "pointer",
+  display: "flex",
+}));
+
+const ImageWrapper = styled("div", () => ({
+  position: "relative",
+  height: "1.3rem",
+  width: "2rem",
+}));
+
+const Text = styled("div", ({ $theme }) => ({
+  ...$theme.typography.font200,
+  position: "relative",
+  height: "1.3rem",
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  marginLeft: "0.5rem",
+  textTransform: "uppercase",
+}));
+
 function BackButton() {
-  const [css] = useStyletron();
   const router = useRouter();
 
   return (
-    <div
-      onClick={router.back}
-      className={css({
-        position: "relative",
-        height: "2.4rem",
-        width: "7rem",
-        cursor: "pointer",
-        display: "flex",
-      })}
-    >
-      <div
-        className={css({
-          position: "absolute",
-          height: "2.4rem",
-          width: "2.4rem",
-        })}
-      >
-        <Image src={leftArrow} alt={"left arrow"} layout={"fill"} />
-      </div>
-      <div
-        className={css({
-          position: "relative",
-          height: "2.4rem",
-          width: "100%",
-          right: "0",
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-          fontSize: "1.3rem",
-          fontWeight: 300,
-          letterSpacing: "1px",
-          marginLeft: "1.5rem",
-          textTransform: "uppercase",
-        })}
-      >
-        Back
-      </div>
-    </div>
+    <Wrapper onClick={router.back}>
+      <ImageWrapper>
+        <Image src={leftArrow} alt={"left arrow"} layout={"fill"} priority />
+      </ImageWrapper>
+      <Text>Back</Text>
+    </Wrapper>
   );
 }
 
