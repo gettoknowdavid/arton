c21bcfe8143cb87d390e19e704643c407a872637
David Michael II <knowdavidmichael@gmail.com>
2022-10-09 16:32:18 +0100

    replace redux with context api

--- src/components/atoms/cart-icon/index.tsx
+++ src/components/atoms/cart-icon/index.tsx
@@ -3,11 +3,18 @@ import { ShoppingCart } from "phosphor-react";
 import { useStyletron } from "baseui";
 import { useRouter } from "next/router";
 import { Block } from "baseui/block";
+import { CartContext } from "../../../contexts/cart.context";
 
 function CartIcon() {
   const [css, theme] = useStyletron();
 
   const router = useRouter();
+  const { state } = React.useContext(CartContext);
+  const [quantity, setQuantity] = React.useState(0);
+
+  React.useEffect(() => {
+    setQuantity(state.items?.map((i) => i.quantity).reduce((a, b) => a + b, 0));
+  }, [state.items]);
 
   return (
     <div
@@ -23,25 +30,27 @@ function CartIcon() {
       <Block display={["flex", "flex", "flex", "none"]}>
         <ShoppingCart />
       </Block>
-      <p
-        className={css({
-          height: "0.75rem",
-          width: "0.75rem",
-          backgroundColor: theme.colors.black,
-          color: theme.colors.white,
-          marginLeft: "0.2rem",
-          fontSize: "0.563rem",
-          fontWeight: 500,
-          letterSpacing: 0,
-          lineHeight: "0.625rem",
-          display: "flex",
-          justifyContent: "center",
-          textAlign: "center",
-          alignItems: "center",
-        })}
-      >
-        80
-      </p>
+      {quantity > 0 ? (
+        <p
+          className={css({
+            height: "0.75rem",
+            width: "0.75rem",
+            backgroundColor: theme.colors.black,
+            color: theme.colors.white,
+            marginLeft: "0.2rem",
+            fontSize: "0.563rem",
+            fontWeight: 500,
+            letterSpacing: 0,
+            lineHeight: "0.625rem",
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            alignItems: "center",
+          })}
+        >
+          {quantity}
+        </p>
+      ) : null}
     </div>
   );
 }
