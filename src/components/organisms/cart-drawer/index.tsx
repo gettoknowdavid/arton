1a0f59a7da364401d8ac118d3856359d232b58bf
David Michael II <knowdavidmichael@gmail.com>
2022-10-09 16:32:42 +0100

    replace redux with context api

--- src/components/organisms/cart-drawer/index.tsx
+++ src/components/organisms/cart-drawer/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { ANCHOR, Drawer } from "baseui/drawer";
-import { StyledBDHeader, StyledBDHeading } from "./cart-drawer.styles";
+import {
+  StyledBDBody,
+  StyledBDFooter,
+  StyledBDFreeShippingText,
+  StyledBDHeader,
+  StyledBDHeading,
+  StyledBDSubtotalValue,
+  StyledBDSubtotalWrapper,
+  StyledCartList,
+} from "./cart-drawer.styles";
+import { CartContext } from "../../../contexts/cart.context";
+import { CartItemInterface } from "../../../types";
+import CartItem from "../../atoms/cart-item";
+import { currency } from "../../../lib/currency-formatter";
+import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
+import { Button, KIND, SIZE } from "baseui/button";
 
 function CartDrawer() {
+  const { state } = React.useContext(CartContext);
+
   return (
     <Drawer
-      isOpen={false}
+      isOpen={state.cartDrawerOpen}
       onClose={() => null}
       autoFocus
       anchor={ANCHOR.right}
@@ -33,42 +50,38 @@ function CartDrawer() {
         <StyledBDHeading>My Cart</StyledBDHeading>
       </StyledBDHeader>
 
-      {/*<StyledBDBody>*/}
-      {/*  <StyledCartList>*/}
-      {/*    {items.map((item: CartItemInterface) => (*/}
-      {/*      <CartItem key={item.id} item={item} />*/}
-      {/*    ))}*/}
-      {/*  </StyledCartList>*/}
+      <StyledBDBody>
+        <StyledCartList>
+          {state.items.map((item: CartItemInterface) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </StyledCartList>
 
-      {/*  <StyledBDFooter>*/}
-      {/*    <StyledBDSubtotalWrapper>*/}
-      {/*      <p>Sub-total</p>*/}
-      {/*      <StyledBDSubtotalValue>*/}
-      {/*        {currency.format(amount)}*/}
-      {/*      </StyledBDSubtotalValue>*/}
-      {/*    </StyledBDSubtotalWrapper>*/}
+        <StyledBDFooter>
+          <StyledBDSubtotalWrapper>
+            <p>Sub-total</p>
+            <StyledBDSubtotalValue>{currency.format(50)}</StyledBDSubtotalValue>
+          </StyledBDSubtotalWrapper>
 
-      {/*    <FlexGrid flexGridColumnCount={2} flexGridColumnGap={"0.5rem"}>*/}
-      {/*      <FlexGridItem>*/}
-      {/*        <Button kind={KIND.secondary} onClick={openCart}>*/}
-      {/*          View Cart*/}
-      {/*        </Button>*/}
-      {/*      </FlexGridItem>*/}
-      {/*      <FlexGridItem>*/}
-      {/*        <Button size={SIZE.compact} disabled={!totalQuantity}>*/}
-      {/*          Checkout*/}
-      {/*        </Button>*/}
-      {/*      </FlexGridItem>*/}
-      {/*    </FlexGrid>*/}
+          <FlexGrid flexGridColumnCount={2} flexGridColumnGap={"0.5rem"}>
+            <FlexGridItem>
+              <Button kind={KIND.secondary} onClick={() => {}}>
+                View Cart
+              </Button>
+            </FlexGridItem>
+            <FlexGridItem>
+              <Button size={SIZE.compact}>Checkout</Button>
+            </FlexGridItem>
+          </FlexGrid>
 
-      {/*    <StyledBDFreeShippingText>*/}
-      {/*      <Button kind={KIND.tertiary} onClick={() => dispatch(clearCart())}>*/}
-      {/*        Clear Cart*/}
-      {/*      </Button>*/}
-      {/*      Free Shipping Worldwide*/}
-      {/*    </StyledBDFreeShippingText>*/}
-      {/*  </StyledBDFooter>*/}
-      {/*</StyledBDBody>*/}
+          <StyledBDFreeShippingText>
+            <Button kind={KIND.tertiary} onClick={() => {}}>
+              Clear Cart
+            </Button>
+            Free Shipping Worldwide
+          </StyledBDFreeShippingText>
+        </StyledBDFooter>
+      </StyledBDBody>
     </Drawer>
   );
 }
