a2892978c6b693bb95b7880e576ae7ccb9b99f09
David Michael II <knowdavidmichael@gmail.com>
2022-10-09 17:49:34 +0100

    refactor styling

--- src/components/atoms/cart-icon/index.tsx
+++ src/components/atoms/cart-icon/index.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { ShoppingCart } from "phosphor-react";
-import { useStyletron } from "baseui";
+import { styled } from "baseui";
 import { useRouter } from "next/router";
 import { Block } from "baseui/block";
 import { CartContext } from "../../../contexts/cart.context";
 
-function CartIcon() {
-  const [css, theme] = useStyletron();
+const Wrapper = styled("div", () => ({
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  height: "100%",
+}));
 
+const Text = styled("p", ({ $theme }) => ({
+  height: "0.75rem",
+  width: "0.75rem",
+  backgroundColor: $theme.colors.black,
+  color: $theme.colors.white,
+  marginLeft: "0.2rem",
+  fontSize: "0.563rem",
+  fontWeight: 400,
+  letterSpacing: 0,
+  lineHeight: "0.625rem",
+  display: "flex",
+  justifyContent: "center",
+  textAlign: "center",
+  alignItems: "center",
+}));
+
+function CartIcon() {
   const router = useRouter();
+
   const { state } = React.useContext(CartContext);
+
   const [quantity, setQuantity] = React.useState(0);
 
   React.useEffect(() => {
@@ -17,41 +40,13 @@ function CartIcon() {
   }, [state.items]);
 
   return (
-    <div
-      onClick={() => router.push("/cart")}
-      className={css({
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        height: "100%",
-      })}
-    >
+    <Wrapper onClick={() => router.push("/cart")}>
       <Block display={["none", "none", "none", "flex"]}>Cart</Block>
       <Block display={["flex", "flex", "flex", "none"]}>
         <ShoppingCart />
       </Block>
-      {quantity > 0 ? (
-        <p
-          className={css({
-            height: "0.75rem",
-            width: "0.75rem",
-            backgroundColor: theme.colors.black,
-            color: theme.colors.white,
-            marginLeft: "0.2rem",
-            fontSize: "0.563rem",
-            fontWeight: 500,
-            letterSpacing: 0,
-            lineHeight: "0.625rem",
-            display: "flex",
-            justifyContent: "center",
-            textAlign: "center",
-            alignItems: "center",
-          })}
-        >
-          {quantity}
-        </p>
-      ) : null}
-    </div>
+      {quantity > 0 ? <Text>{quantity}</Text> : null}
+    </Wrapper>
   );
 }
 
