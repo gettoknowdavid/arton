c4494430446bcfd799a0c7a92ce78e36a1e5cb19
David Michael II <knowdavidmichael@gmail.com>
2022-10-16 18:36:06 +0100

    Create validators.tsx

---
+++ src/lib/validators.tsx
@@ -0,0 +1,6 @@
+export const validateEmail = (email: string) =>
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+export const validatePhone = (phone: string) =>
+  /^(\+234|234|0)(70|80|81|91|90)([0-9]{8})$/.test(phone);
