abde6651d6e2dd96c5fa51731d278d522707cbe6
David Michael II <knowdavidmichael@gmail.com>
2022-10-17 13:34:32 +0100

    implement sort filter

--- src/components/molecules/sort-filter/index.tsx
+++ src/components/molecules/sort-filter/index.tsx
@@ -5,18 +5,47 @@ import {
   StyledSortFTitle,
   StyledSortFWrapper,
 } from "./sort-filter.styles";
+import { FilterActionType } from "../../../types";
+import { FilterContext } from "../../../contexts/filter.context";
+
 const SORT_LIST = [
   { id: 0, name: "Price low to high" },
   { id: 1, name: "Price high to low" },
 ];
 
 function SortFilter() {
+  const { dispatch, state } = React.useContext(FilterContext);
+
+  const sortAsc = () => {
+    dispatch({
+      type: FilterActionType.SORT_PRICE_ASC,
+      payload: { products: state.filteredProducts, sortIndex: 0 },
+    });
+  };
+
+  const sortDesc = () => {
+    dispatch({
+      type: FilterActionType.SORT_PRICE_DESC,
+      payload: { products: state.filteredProducts, sortIndex: 1 },
+    });
+  };
+
   return (
     <StyledSortFWrapper>
       <StyledSortFTitle>SORT</StyledSortFTitle>
       <StyledSortFList>
         {SORT_LIST.map((ITEM) => (
-          <StyledSortFListItem key={ITEM.id}>
+          <StyledSortFListItem
+            key={ITEM.id}
+            $isActive={ITEM.id === state.sortIndex}
+            onClick={() => {
+              if (ITEM.id === 0) {
+                sortAsc();
+              } else {
+                sortDesc();
+              }
+            }}
+          >
             <p>{ITEM.name}</p>
           </StyledSortFListItem>
         ))}
