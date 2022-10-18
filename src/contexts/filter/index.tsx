a71ef8d6abeda462f51550aa3b389104418fa51f
David Michael II <knowdavidmichael@gmail.com>
2022-10-18 12:21:42 +0100

    create filter context index

---
+++ src/contexts/filter/index.tsx
@@ -0,0 +1,17 @@
+import { FilterProvider } from "./filter.provider";
+import { FilterContext } from "./filter.context";
+import {
+  sortByCategory,
+  sortByPrice,
+  sortBySize,
+  getAllProducts,
+} from "./filter.actions";
+
+export {
+  FilterContext,
+  FilterProvider,
+  sortByCategory,
+  sortByPrice,
+  sortBySize,
+  getAllProducts,
+};
