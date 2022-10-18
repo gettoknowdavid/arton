c0ddd3aa198c67ffcf45e576ccc16144c8c704fa
David Michael II <knowdavidmichael@gmail.com>
2022-10-18 12:21:58 +0100

    fix sizes list type

--- src/lib/sizes.tsx
+++ src/lib/sizes.tsx
@@ -1,8 +1,11 @@
-export const SIZES = [
-  { label: "XS - Extra Small", id: "XS" },
-  { label: "S - Small", id: "S" },
-  { label: "M - Medium", id: "M" },
-  { label: "L - Large", id: "L" },
-  { label: "XL - Extra Large", id: "XL" },
-  { label: "XXL - Extra Extra Large", id: "XXL" },
+import { SizeType } from "../types";
+
+export const SIZES: SizeType[] = [
+  { id: 0, attributes: { title: "XS - Extra Small", sizeCode: "XS" } },
+  { id: 1, attributes: { title: "S - Small", sizeCode: "S" } },
+  { id: 2, attributes: { title: "M - Medium", sizeCode: "M" } },
+  { id: 3, attributes: { title: "L - Large", sizeCode: "L" } },
+  { id: 4, attributes: { title: "XL - Extra Large", sizeCode: "XL" } },
+  { id: 5, attributes: { title: "XXL - Extra Extra Large", sizeCode: "XXL" } },
+  { id: 6, attributes: { title: "U - Universal", sizeCode: "U" } },
 ];
