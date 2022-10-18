25e5a71015ca31d68b8517d5ab9ea344e89720b3
David Michael II <knowdavidmichael@gmail.com>
2022-10-18 12:21:13 +0100

    refactor

---
+++ src/contexts/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import GlobalProvider from "./global.context";
+import CartProvider from "./cart.context";
+import CheckoutProvider from "./checkout.context";
+import { FilterProvider } from "./filter";
+
+type Props = {
+  children: ReactNode;
+};
+
+function ArtonProvider(props: Props) {
+  return (
+    <GlobalProvider>
+      <CartProvider>
+        <CheckoutProvider>
+          <FilterProvider>{props.children}</FilterProvider>
+        </CheckoutProvider>
+      </CartProvider>
+    </GlobalProvider>
+  );
+}
+
+export default ArtonProvider;
