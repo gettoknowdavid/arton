4f1034e52804a60bdd7d5d73b367b6a4f0566253
David Michael II <knowdavidmichael@gmail.com>
2022-10-18 13:06:02 +0100

    add variant field

--- src/graphql/queries/filter-products.query.tsx
+++ src/graphql/queries/filter-products.query.tsx
@@ -3,10 +3,16 @@ import { ProductFragment } from "../fragments";
 
 export const FilterProductsQuery = gql`
   ${ProductFragment}
-  query FilterProductsQuery($catID: ID, $sort: String, $sizeID: ID) {
+  query FilterProductsQuery(
+    $catID: ID
+    $sort: String
+    $sizeID: ID
+    $variant: String!
+  ) {
     products(
       sort: [$sort]
       filters: {
+        variant: { eq: $variant }
         category: { id: { eq: $catID } }
         sizes: { id: { eq: $sizeID } }
       }
