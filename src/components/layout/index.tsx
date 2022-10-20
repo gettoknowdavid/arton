b86cfa7490f6d64cc15dde7ba4b25bd3a2194969
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 21:15:48 +0100

    fix layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,6 +5,9 @@ import Footer from "../organisms/footer";
 import { motion } from "framer-motion";
 import BagDrawer from "../organisms/cart-drawer";
 import Drawer from "../organisms/drawer";
+import SearchBox from "../atoms/search-box";
+import { useOutsideClick } from "../../hooks";
+import { closeSearchBox, SearchContext } from "../../contexts/search";
 
 function Layout({ children }: { children: ReactNode }) {
   const variants = {
@@ -13,11 +16,24 @@ function Layout({ children }: { children: ReactNode }) {
     exit: { opacity: 0, x: 0, y: -100 },
   };
 
+  const { dispatch, state } = React.useContext(SearchContext);
+
+  const action = () => {
+    if (state.searchBoxOpen) {
+      closeSearchBox({ dispatch });
+    }
+  };
+
+  const { ref } = useOutsideClick(action);
+
   return (
     <StyledLayout>
       <Drawer />
       <BagDrawer />
-      <Header />
+      <div ref={ref}>
+        <Header />
+        <SearchBox />
+      </div>
       <motion.div
         variants={variants} // Pass the variant object into Framer Motion
         initial="hidden" // Set the initial state to variants.hidden
