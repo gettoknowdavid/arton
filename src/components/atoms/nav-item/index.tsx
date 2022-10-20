c4bbf1d3b1016fe3af8cd9b3b267f832ca0a856d
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 21:13:10 +0100

    close search box on click

--- src/components/atoms/nav-item/index.tsx
+++ src/components/atoms/nav-item/index.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 import { StyledNavA, StyledNavItem } from "./nav-item.styles";
 import { NavItemType } from "../../../types";
 import { useRouter } from "next/router";
+import { closeSearchBox, SearchContext } from "../../../contexts/search";
 
 function NavItem({ item }: { item: NavItemType }) {
   const router = useRouter();
+  const { dispatch } = React.useContext(SearchContext);
 
   return (
-    <StyledNavItem>
+    <StyledNavItem onClick={() => closeSearchBox({ dispatch })}>
       <Link href={`/${item.slug}`}>
         <StyledNavA $isActive={router.pathname === `/${item.slug}`}>
           {item.title}
