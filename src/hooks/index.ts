b2dbb6310f2aff3b615378d07e80406232756071
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 21:59:19 +0100

    fix lint errors

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,13 +1,5 @@
 import { useLocalStorage } from "./use-local-storage";
 import { useOutsideClick } from "./use-outside-click";
 import { useMediaQuery } from "./use-media-query";
-import { useRootDispatch } from "./use-root-dispatch";
-import { useRootSelector } from "./use-root-selector";
 
-export {
-  useLocalStorage,
-  useOutsideClick,
-  useMediaQuery,
-  useRootDispatch,
-  useRootSelector,
-};
+export { useLocalStorage, useOutsideClick, useMediaQuery };
--- src/hooks/use-root-dispatch.ts
+++
@@ -1,4 +0,0 @@
-import { RootDispatch } from "../store";
-import { useDispatch } from "react-redux";
-
-export const useRootDispatch: () => RootDispatch = useDispatch;
