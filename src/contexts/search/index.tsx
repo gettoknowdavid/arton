4d538cc0fe10f289668982089273a8a9797a1c24
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 21:32:08 +0100

    update search

---
+++ src/contexts/search/index.tsx
@@ -0,0 +1,17 @@
+import { SearchContext } from "./search.context";
+import { SearchProvider } from "./search.provider";
+import {
+  openSearchBox,
+  closeSearchBox,
+  productSearch,
+  toggleSearchBox,
+} from "./search.actions";
+
+export {
+  SearchContext,
+  SearchProvider,
+  productSearch,
+  openSearchBox,
+  closeSearchBox,
+  toggleSearchBox,
+};
