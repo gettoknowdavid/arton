29b6fd72b538f25a7ba176be2d3ae2421da4358a
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 21:12:19 +0100

    close search box on click

--- src/components/atoms/hamburger/index.tsx
+++ src/components/atoms/hamburger/index.tsx
@@ -3,13 +3,23 @@ import { List, X } from "phosphor-react";
 import { Block } from "baseui/block";
 import { StyledHamburger } from "./hamburger.styles";
 import { GlobalContext } from "../../../contexts/global.context";
+import { closeSearchBox, SearchContext } from "../../../contexts/search";
 
 function Hamburger() {
+  const { dispatch, state } = React.useContext(SearchContext);
+
   return (
     <GlobalContext.Consumer>
       {({ toggleDrawer, drawerOpen }) => (
         <Block display={["flex", "flex", "flex", "none"]}>
-          <StyledHamburger onClick={toggleDrawer}>
+          <StyledHamburger
+            onClick={() => {
+              if (state.searchBoxOpen) {
+                closeSearchBox({ dispatch });
+              }
+              toggleDrawer();
+            }}
+          >
             {drawerOpen ? <X /> : <List />}
           </StyledHamburger>
         </Block>
