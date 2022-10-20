3055f5a51a71b3e94ed2584efbb2d314d95c6b77
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 23:46:21 +0100

    fix mobile view

--- src/components/atoms/logo/logo.styles.tsx
+++ src/components/atoms/logo/logo.styles.tsx
@@ -1,12 +1,14 @@
 import { styled } from "baseui";
 
-export const StyledLogo = styled("div", () => ({
+export const StyledLogo = styled("div", ({ $theme }) => ({
   display: "block",
-  height: "2.8125rem",
+  height: "2rem",
   left: "50%",
   position: "absolute",
   transform: "translateX(-50%)",
   WebkitTransform: "translateX(-50%)",
-  width: "8.5625rem",
-  zIndex: 1022,
+  width: "7.7rem",
+  zIndex: 200,
+  [$theme.mediaQuery.medium]: { height: "2.8125rem", width: "8.5625rem" },
+  [$theme.mediaQuery.large]: { height: "2.8125rem", width: "8.5625rem" },
 }));
