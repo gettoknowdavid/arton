25886674b9d48ac7679c5c38b716178afba7c380
David Michael II <knowdavidmichael@gmail.com>
2022-10-20 21:32:00 +0100

    update hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,13 @@
 import { useLocalStorage } from "./use-local-storage";
+import { useOutsideClick } from "./use-outside-click";
+import { useMediaQuery } from "./use-media-query";
 import { useRootDispatch } from "./use-root-dispatch";
 import { useRootSelector } from "./use-root-selector";
 
-export { useLocalStorage, useRootDispatch, useRootSelector };
+export {
+  useLocalStorage,
+  useOutsideClick,
+  useMediaQuery,
+  useRootDispatch,
+  useRootSelector,
+};
